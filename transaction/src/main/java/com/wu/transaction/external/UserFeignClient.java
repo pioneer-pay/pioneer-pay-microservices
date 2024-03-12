63d7fe329fe1b5ef8a454acf37db84b78b6f724d
neeta <neaundhe@altimetrik.com>
2024-03-12 12:07:11 +0530

    Added Junit text cases for email service

--- transaction/src/main/java/com/wu/transaction/external/UserFeignClient.java
+++ transaction/src/main/java/com/wu/transaction/external/UserFeignClient.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @FeignClient(name="USER-INFO-SERVICE")
 public interface UserFeignClient {
     @GetMapping("/api/user/get/email/{userId}")
